db28a0030559bf7e43e7d4b19a7a89482aff853f
T480s <techcom.ec20@gmail.com>
2021-01-31 10:44:55 +0700

    Add Router

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Msg: {{ msg }}</div>
+  <div>Object: {{ obj }}</div>
+  <div>Objdec:{{ obj2 }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const msg = ref("Test");
+    const obj = reactive({ id: 1, name: "name", last: "last" });
+    const obj2 = reactive({ id: 1, name: "name", last: "last" });
+    return { msg, obj, obj2 };
+  },
+});
+</script>
+
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
-// TypeScript error? Run VSCode command
-// TypeScript: Select TypeScript version - > Use Workspace Version
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import AppLayout from './components/layout/AppLayout.vue';
+import EmptyLayout from './components/layout/EmptyLayout.vue';
+
+const app = createApp(App)
+app.component('default-layout', AppLayout);
+app.component('empty-layout', EmptyLayout);
+app.use(router)
+app.mount('#app')
